d4c70f11f8c8879c73fb2a6d964ffa1110d48741
weipeng <wwp.shanghai@gmail.com>
2023-06-29 15:32:06 +0800

    use runtime path (#5)
    * feat: first commit
    
    * feat: modify desc
    
    * feat: use relative path
    
    * feat: fix build error
    
    * feat: use runtime path
    
    ---------
    
    Co-authored-by: quanzheng <wwp258714@antgroup.com>

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,5 +5,14 @@ export default defineConfig({
     name: 'home',
   },
   runtimePublicPath: {},
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  scripts: [
+    `
+    if (window.location.hostname === 'alipay.github.io' ) {
+      window.publicPath = '/fury-sites/';
+    } else {
+      window.publicPath = '/';
+    }
+    `
+  ]
 });
