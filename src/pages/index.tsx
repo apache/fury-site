657ec021eaf44664787e9ae032440a0a306ee852
PragmaTwice <twice@apache.org>
2023-12-27 21:57:30 +0900

    Add more links and refine words in footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,8 +45,8 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Layout
-        title={`Hello from ${siteConfig.title}`}
-        description="Description will go into a meta tag in <head />">
+        title={`${siteConfig.title}`}
+        description="@{siteConfig.tagline}">
         <HomepageHeader />
         <main>
           <HomepageFeatures />
