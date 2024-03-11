155e20690c8dea6fb96ed811e8b3fbcafc610735
PragmaTwice <twice@apache.org>
2024-03-12 00:02:12 +0900

    Remove github star iframe since it's broken

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,15 +25,6 @@ function HomepageHeader() {
           <Link className="button button--secondary button--lg" to="/docs/start/install">
             Get Started
           </Link>
-          <span className={styles.indexCtasGitHubButtonWrapper}>
-            <iframe
-              className={styles.indexCtasGitHubButton}
-              src="https://ghbtns.com/github-btn.html?user=apache&amp;repo=incubator-fury&amp;type=star&amp;count=true&amp;size=large"
-              width={160}
-              height={30}
-              title="GitHub Stars"
-            />
-          </span>
         </div>
       </div>
     </header>
