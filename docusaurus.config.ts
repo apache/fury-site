aed3b4034a7bf9b7a33b528afe2d62ad2c187f94
chaokunyang <shawn.ck.yang@gmail.com>
2024-04-02 11:17:28 +0800

    add_spec_tab

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,6 +78,12 @@ const config: Config = {
           position: 'right',
           label: 'Guide',
         },
+        {
+          type: 'docSidebar',
+          sidebarId: 'specSidebar',
+          position: 'right',
+          label: 'Specification',
+        },
         {to: '/blog', label: 'Blog', position: 'right'},
         {
           type: 'dropdown',
