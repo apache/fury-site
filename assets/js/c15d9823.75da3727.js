b80ce25b069dd41945bd9c0ba53002f98c62af56
chaokunyang <chaokunyang@users.noreply.github.com>
2026-08-07 08:10:03 +0000

    deploy: 0ffd2a8d79cb1085823b4834f65f1c3b476d3cd8

--- assets/js/c15d9823.75da3727.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfory_site=self.webpackChunkfory_site||[]).push([["46431"],{29328(e){e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":5,"totalCount":49,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- zh-CN/assets/js/a3570b99.99be142f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfory_site=self.webpackChunkfory_site||[]).push([["58800"],{57566(e){e.exports=JSON.parse('{"metadata":{"permalink":"/zh-CN/blog/page/2","page":2,"postsPerPage":10,"totalPages":5,"totalCount":49,"previousPage":"/zh-CN/blog","nextPage":"/zh-CN/blog/page/3","blogDescription":"\u535A\u5BA2","blogTitle":"\u535A\u5BA2"}}')}}]);
